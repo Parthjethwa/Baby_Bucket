66cc742dce19d68e4e1586d734b66e8e518ea732
Parth <parthjethwa0999@gmail.com>
2022-11-29 18:22:44 +0530

    first commit

---
+++ android/app/src/main/kotlin/eShop/multivendor/customer/eshop_multivendor/MainActivity.kt
@@ -0,0 +1,8 @@
+package pinko.application.customer
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
+
---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,32 @@
+const String appName = 'Baby Bucket';
+
+const String packageName = 'pinko.application.customer';
+const String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+const String iosPackage = 'pinko.application.customer';
+const String iosLink = 'your ios link here';
+const String appStoreId = '123456789';
+
+const String deepLinkUrlPrefix = 'https://pinko.page.link';
+const String deepLinkName = 'PinkoImplex';
+
+const int timeOut = 50;
+const int perPage = 10;
+
+const double textFontSize10 = 10;
+const double textFontSize12 = 12;
+const double textFontSize14 = 14;
+const double textFontSize16 = 16;
+
+const double circularBorderRadius5 = 5;
+const double circularBorderRadius7 = 7;
+const double circularBorderRadius10 = 10;
+
+const String baseUrl = 'https://testbud.in/pinko/app/v1/api/';
+const String jwtKey = 'a73ef3944849be1a6b85bde0c5f5858721fa101e';
+
+
+const String BANK_DETAIL = 'Bank Details:\n' +
+    'Account No :123XXXXX\n' +
+    'IFSC Code: 123XXX \n' +
+    'Name: Abc xyz';
